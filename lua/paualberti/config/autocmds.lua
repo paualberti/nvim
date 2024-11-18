e574665f4ddd45e3abeb12d6ee569fb421da54f2
paualberti <paualberti73@gmail.com>
2024-11-18 09:24:56 +0100

    autosave in markdown

--- lua/paualberti/config/autocmds.lua
+++ lua/paualberti/config/autocmds.lua
@@ -9,14 +9,18 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	pattern = { "*.*" },
 	callback = function()
 		local save_cursor = vim.fn.getpos(".")
-		pcall(function() vim.cmd([[%s/\s\+$//e]]) end)
+		pcall(function()
+			vim.cmd([[%s/\s\+$//e]])
+		end)
 		vim.fn.setpos(".", save_cursor)
 	end,
 })
 
 -- highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function() vim.highlight.on_yank() end,
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- go to last location when opening a buffer
@@ -26,6 +30,16 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 		local lcount = vim.api.nvim_buf_line_count(0)
 		if mark[1] > 0 and mark[1] <= lcount then
 			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
+
+-- autosave markdown files
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = "*.md",
+	callback = function()
+		if vim.fn.exists(":ASToggle") == 2 then
+			vim.cmd("ASToggle")
 		end
 	end,
 })
