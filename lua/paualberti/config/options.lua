4e4de17b8c79e6eee07dbd08fae9577693401891
paualberti <paualberti73@gmail.com>
2024-12-01 00:21:42 +0100

    eyeliner

--- lua/paualberti/config/options.lua
+++ lua/paualberti/config/options.lua
@@ -49,6 +49,6 @@ vim.opt.splitbelow = true
 vim.opt.autochdir = false
 vim.opt.iskeyword:append("-")
 vim.opt.mouse = ""
-vim.opt.clipboard = ""
+vim.opt.clipboard = "unnamedplus"
 vim.opt.modifiable = true
 vim.opt.encoding = "UTF-8"
